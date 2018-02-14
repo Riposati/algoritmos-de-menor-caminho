67d0977f148e45d0ac8c1db79a67319d08e7bdc8
Gustavo Riposati <guriposa@gmail.com>
2018-02-14 18:21:39 -0200

    Algoritmos SSP - melhor caminho fonte unica
    Algoritmos SSP - melhor caminho fonte unica

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		int tam = 3;
-		
+
 		Vertice vs[] = new Vertice[tam];
 		char vet[] = new char[tam];
-//		vet = new char[] { 's', 't', 'x', 'z', 'y' }; // vetor para os exemplos do livro do cormen, OBS tam = 5*
-		vet = new char[] {'u','v','w'}; // vetor para o exemplo do ciclo negativo, OBS tam = 3*
+		// vet = new char[] { 's', 't', 'x', 'z', 'y' }; // vetor para os exemplos do
+		// livro do cormen, OBS tam = 5*
+		vet = new char[] { 'u', 'v', 'w' }; // vetor para o exemplo do ciclo negativo, OBS tam = 3*
 
 		Grafo g = new Grafo();
 
@@ -44,22 +45,22 @@ public class Main {
 		g.addAresta(vs[0], vs[1], 3);
 		g.addAresta(vs[1], vs[2], 3);
 		g.addAresta(vs[1], vs[0], -5);
-		
+
 		// exemplo do Bellman-Ford livro Cormen
-//		g.addAresta(vs[0], vs[1], 6);
-//		g.addAresta(vs[0], vs[4], 7);
-//
-//		g.addAresta(vs[1], vs[2], 5);
-//		g.addAresta(vs[1], vs[3], -4);
-//		g.addAresta(vs[1], vs[4], 8);
-//
-//		g.addAresta(vs[2], vs[1], -2);
-//
-//		g.addAresta(vs[3], vs[2], 7);
-//		g.addAresta(vs[3], vs[0], 2);
-//
-//		g.addAresta(vs[4], vs[2], -3);
-//		g.addAresta(vs[4], vs[3], 9);
+		// g.addAresta(vs[0], vs[1], 6);
+		// g.addAresta(vs[0], vs[4], 7);
+		//
+		// g.addAresta(vs[1], vs[2], 5);
+		// g.addAresta(vs[1], vs[3], -4);
+		// g.addAresta(vs[1], vs[4], 8);
+		//
+		// g.addAresta(vs[2], vs[1], -2);
+		//
+		// g.addAresta(vs[3], vs[2], 7);
+		// g.addAresta(vs[3], vs[0], 2);
+		//
+		// g.addAresta(vs[4], vs[2], -3);
+		// g.addAresta(vs[4], vs[3], 9);
 
 		// exemplo do Dijkstra livro Cormen
 
